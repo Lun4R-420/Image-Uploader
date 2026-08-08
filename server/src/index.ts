b44392fdbb1b20099f165e736353a46be8815271
Lun4R-420 <adelynaspoiala@gmail.com>
2026-08-08 17:58:58 +0300

    Deployment changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,34 +1,25 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+
 import userRoutes from "./routes/users.js";
 import uploadRoutes from "./routes/upload.js";
-import cors from "cors";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://image-uploader-silk-seven.vercel.app"
+}));
+
 app.use(express.json());
-
-app.get("/", (req, res) => {
-    res.json({ message: "Backend is alive!" });
-});
-
-app.use("/uploads", express.static("uploads"));
 
 app.use("/users", userRoutes);
 app.use("/upload", uploadRoutes);
-
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.status(400).json({
-        message: err.message
-    });
-});
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
-
 
 export default app;
