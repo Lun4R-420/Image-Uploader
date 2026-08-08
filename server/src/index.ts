2a95cd39bbf0e93564e22f86942aff70894f94d7
Lun4R-420 <adelynaspoiala@gmail.com>
2026-08-08 16:25:50 +0300

    Prepare backend for deployment

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,6 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
-const PORT = 3000;
 const users = [
     {id: 1, name: "Ade"}
 ];
@@ -17,12 +16,10 @@ app.use("/users", userRoutes);
 
 app.use("/upload", uploadRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
-
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
     res.status(400).json({
         message: err.message
     });
 });
+
+export default app;
